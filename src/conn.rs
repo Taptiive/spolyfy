be57a0441375508e8d109bf784cc6f83332a6087
Taptiive <bastian.v2745@gmail.com>
2021-10-17 21:26:00 -0300

    Second commit

--- src/conn.rs
+++ src/conn.rs
@@ -1,5 +1,6 @@
-use dbus::arg::PropMap;
 use dbus::blocking::{Connection as DbusCon, Proxy};
+use std::collections::HashMap;
+use dbus::arg::{RefArg, Variant};
 use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
 
 type GenericResult<R> = Result<R, dbus::Error>;
@@ -8,38 +9,41 @@ pub(crate) trait SpotifyConn{
     fn play_pause(&self)-> GenericResult<()>;
     fn next(&self) -> GenericResult<()>;
     fn previous(&self) -> GenericResult<()>;
-    fn metadata(&self) -> GenericResult<PropMap>;
+    fn get_metadata(&self) -> GenericResult<PropMap>;
     fn artist(&self) -> GenericResult<String>;
     fn title(&self) -> GenericResult<String>;
 }
 
+type PropMap = HashMap<String, Variant<Box<dyn RefArg>>>;
+
 impl<'r> SpotifyConn for Proxy<'r, &'r DbusCon>{
     fn play_pause(&self) -> GenericResult<()>{
-        self.method_call("org.mpris.MediaPlayer2.spotify", "org.mpris.MediaPlayer2.Player.PlayPause", ())
+        self.method_call("org.mpris.MediaPlayer2.Player", "PlayPause", ())
     }
     fn next(&self) -> GenericResult<()>{
-        self.method_call("org.mpris.MediaPlayer2.spotify", "org.mpris.MediaPlayer2.Player.Next", ())
+        self.method_call("org.mpris.MediaPlayer2.Player", "Next", ())
     }
     fn previous(&self) -> GenericResult<()>{
-        self.method_call("org.mpris.MediaPlayer2.spotify", "org.mpris.MediaPlayer2.Player.Previous", ())
+        self.method_call("org.mpris.MediaPlayer2.Player", "Previous", ())
     }
-    fn metadata(&self) -> GenericResult<PropMap> {
+    fn get_metadata(&self) -> GenericResult<PropMap>{
         self.get("org.mpris.MediaPlayer2.Player", "Metadata")
+
     }
     fn artist(&self) -> GenericResult<String>{
-        let data = self.metadata()?;
-        let artists = data.get("xesam:artist").ok_or(dbus::Error::new_custom("err", "Empty artists list"))?
+        let data = self.get_metadata()?;
+        let artists = data.get("xesam:artist").ok_or(dbus::Error::new_failed("Empty artists list"))?
             .0.as_iter().ok_or(
-            dbus::Error::new_custom("err", "Could not iterate over artists")
+            dbus::Error::new_failed("Could not iterate over artists")
         )?.map(|a|a.as_str().unwrap()).collect::<Vec<&str>>();
         Ok(artists.join(", ").trim_end_matches(", ").to_string())
     }
     fn title(&self) -> GenericResult<String>{
-        let data = self.metadata()?;
+        let data = self.get_metadata()?;
         let title = data.get("xesam:title").ok_or(
-            dbus::Error::new_custom("err", "Failed to retrieve title")
+            dbus::Error::new_failed("Failed to retrieve title")
         )?.0.as_str().ok_or(
-            dbus::Error::new_custom("err", "Failed to transform into str")
+            dbus::Error::new_failed("Failed to transform into str")
         )?;
         Ok(title.to_string())
     }
